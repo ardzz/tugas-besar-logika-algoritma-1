9b2728fcb1068b9706b00b20c5ad88ff9ec2631b
reky <me@reky.dev>
2022-12-14 02:54:16 +0700

    Add independent variables (6 variables)

--- regression.c
+++ regression.c
@@ -1,71 +1,98 @@
 //
-// Created by Naufal Reky Ardhana on 21/11/22.
+// Created by NOKaufal Reky Ardhana on 21/11/22.
 //
 #include "regression.h"
 #include "statistics.h"
 #include "math.h"
 
-double b1(struct multiple_linear_regression *multiple_linear_regression){
-    double x, y, sigma_x1, sigma_x2, sigma_x1x2, sigma_x1y, sigma_x2y;
+int linear_regression(
+        const double *x1, const double *x2, const double *x3,
+        const double *x4, const double *x5, const double *x6,
+        double *y, struct multiple_linear_regression *multiple_linear_regression){
 
-    sigma_x1   = multiple_linear_regression->sigma_x.x1.sigma_of_sigma;
-    sigma_x2   = multiple_linear_regression->sigma_x.x2.sigma_of_sigma;
-    sigma_x1x2 = multiple_linear_regression->sigma_x.x1x2.sigma_of_sigma;
-    sigma_x1y  = multiple_linear_regression->sigma_xy.x1y_sigma_of_sigma;
-    sigma_x2y  = multiple_linear_regression->sigma_xy.x2y_sigma_of_sigma;
-
-    x = (sigma_x2 * sigma_x1y) - (sigma_x2y * sigma_x1x2);
-    y = (sigma_x1 * sigma_x2) - (sigma_x1x2 * sigma_x1x2);
-
-    return x / y;
-}
-
-double b2(struct multiple_linear_regression *multiple_linear_regression){
-    double x, y, sigma_x1, sigma_x2, sigma_x1x2, sigma_x1y, sigma_x2y;
-
-    sigma_x1   = multiple_linear_regression->sigma_x.x1.sigma_of_sigma;
-    sigma_x2   = multiple_linear_regression->sigma_x.x2.sigma_of_sigma;
-    sigma_x1x2 = multiple_linear_regression->sigma_x.x1x2.sigma_of_sigma;
-    sigma_x1y  = multiple_linear_regression->sigma_xy.x1y_sigma_of_sigma;
-    sigma_x2y  = multiple_linear_regression->sigma_xy.x2y_sigma_of_sigma;
-
-    x = (sigma_x1 * sigma_x2y) - (sigma_x1y * sigma_x1x2);
-    y = (sigma_x1 * sigma_x2) - (sigma_x1x2 * sigma_x1x2);
-    return x / y;
-}
-
-double b0(struct multiple_linear_regression *multiple_linear_regression){
-    double sigma_x1, sigma_x2, sigma_y, b1, b2, n;
-
-    sigma_x1   = multiple_linear_regression->sigma_x.x1.sigma_of_sigma;
-    sigma_x2   = multiple_linear_regression->sigma_x.x2.sigma_of_sigma;
-    sigma_y    = multiple_linear_regression->sigma_y.y.sigma_of_sigma;
-    b1         = multiple_linear_regression->result.b1;
-    b2         = multiple_linear_regression->result.b2;
-    n          = multiple_linear_regression->n;
-
-    return (sigma_y - (b1 * sigma_x1) - (b2 * sigma_x2)) / n;
-}
-
-int linear_regression(const double *x1, const double *x2, double *y, struct multiple_linear_regression *multiple_linear_regression){
     struct sigma_x *sigma_x = &multiple_linear_regression->sigma_x;
     struct sigma_y *sigma_y = &multiple_linear_regression->sigma_y;
     struct sigma_xy *sigma_xy = &multiple_linear_regression->sigma_xy;
 
+    double b0, b1, b2, b3, b4, b5, b6;
+
     sigma_y->y.sum = sum(y, multiple_linear_regression->n);
     sigma_y->y.square_sum = square_sum(y, multiple_linear_regression->n);
     sigma_y->y.sigma_of_sigma = sigma_of_sigma(sigma_y->y.square_sum, sigma_y->y.sum, multiple_linear_regression->n);
+    sigma_y->y.average = sigma_y->y.sum / multiple_linear_regression->n;
 
     sigma_x->x1.sum = sum(x1, multiple_linear_regression->n);
     sigma_x->x1.square_sum = square_sum(x1, multiple_linear_regression->n);
     sigma_x->x1.sigma_of_sigma = sigma_of_sigma(sigma_x->x1.square_sum, sigma_x->x1.sum, multiple_linear_regression->n);
+    sigma_x->x1.average = sigma_x->x1.sum / multiple_linear_regression->n;
 
     sigma_x->x2.sum = sum(x2, multiple_linear_regression->n);
     sigma_x->x2.square_sum = square_sum(x2, multiple_linear_regression->n);
     sigma_x->x2.sigma_of_sigma = sigma_of_sigma(sigma_x->x2.square_sum, sigma_x->x2.sum, multiple_linear_regression->n);
+    sigma_x->x2.average = sigma_x->x2.sum / multiple_linear_regression->n;
+
+    sigma_x->x3.sum = sum(x3, multiple_linear_regression->n);
+    sigma_x->x3.square_sum = square_sum(x3, multiple_linear_regression->n);
+    sigma_x->x3.sigma_of_sigma = sigma_of_sigma(sigma_x->x3.square_sum, sigma_x->x3.sum, multiple_linear_regression->n);
+    sigma_x->x3.average = sigma_x->x3.sum / multiple_linear_regression->n;
+
+    sigma_x->x4.sum = sum(x4, multiple_linear_regression->n);
+    sigma_x->x4.square_sum = square_sum(x4, multiple_linear_regression->n);
+    sigma_x->x4.sigma_of_sigma = sigma_of_sigma(sigma_x->x4.square_sum, sigma_x->x4.sum, multiple_linear_regression->n);
+    sigma_x->x4.average = sigma_x->x4.sum / multiple_linear_regression->n;
+
+    sigma_x->x5.sum = sum(x5, multiple_linear_regression->n);
+    sigma_x->x5.square_sum = square_sum(x5, multiple_linear_regression->n);
+    sigma_x->x5.sigma_of_sigma = sigma_of_sigma(sigma_x->x5.square_sum, sigma_x->x5.sum, multiple_linear_regression->n);
+
+    sigma_x->x6.sum = sum(x6, multiple_linear_regression->n);
+    sigma_x->x6.square_sum = square_sum(x6, multiple_linear_regression->n);
+    sigma_x->x6.sigma_of_sigma = sigma_of_sigma(sigma_x->x6.square_sum, sigma_x->x6.sum, multiple_linear_regression->n);
 
     sigma_x->x1x2.sum = sum_multi_value(x1, x2, multiple_linear_regression->n);
     sigma_x->x1x2.sigma_of_sigma = sigma_x1x2(sigma_x->x1x2.sum, sigma_x->x1.sum, sigma_x->x2.sum, multiple_linear_regression->n);
+
+    sigma_x->x1x3.sum = sum_multi_value(x1, x3, multiple_linear_regression->n);
+    sigma_x->x1x3.sigma_of_sigma = sigma_x1x2(sigma_x->x1x3.sum, sigma_x->x1.sum, sigma_x->x3.sum, multiple_linear_regression->n);
+
+    sigma_x->x1x4.sum = sum_multi_value(x1, x4, multiple_linear_regression->n);
+    sigma_x->x1x4.sigma_of_sigma = sigma_x1x2(sigma_x->x1x4.sum, sigma_x->x1.sum, sigma_x->x4.sum, multiple_linear_regression->n);
+
+    sigma_x->x1x5.sum = sum_multi_value(x1, x5, multiple_linear_regression->n);
+    sigma_x->x1x5.sigma_of_sigma = sigma_x1x2(sigma_x->x1x5.sum, sigma_x->x1.sum, sigma_x->x5.sum, multiple_linear_regression->n);
+
+    sigma_x->x1x6.sum = sum_multi_value(x1, x6, multiple_linear_regression->n);
+    sigma_x->x1x6.sigma_of_sigma = sigma_x1x2(sigma_x->x1x6.sum, sigma_x->x1.sum, sigma_x->x6.sum, multiple_linear_regression->n);
+
+    sigma_x->x2x3.sum = sum_multi_value(x2, x3, multiple_linear_regression->n);
+    sigma_x->x2x3.sigma_of_sigma = sigma_x1x2(sigma_x->x2x3.sum, sigma_x->x2.sum, sigma_x->x3.sum, multiple_linear_regression->n);
+
+    sigma_x->x2x4.sum = sum_multi_value(x2, x4, multiple_linear_regression->n);
+    sigma_x->x2x4.sigma_of_sigma = sigma_x1x2(sigma_x->x2x4.sum, sigma_x->x2.sum, sigma_x->x4.sum, multiple_linear_regression->n);
+
+    sigma_x->x2x5.sum = sum_multi_value(x2, x5, multiple_linear_regression->n);
+    sigma_x->x2x5.sigma_of_sigma = sigma_x1x2(sigma_x->x2x5.sum, sigma_x->x2.sum, sigma_x->x5.sum, multiple_linear_regression->n);
+
+    sigma_x->x2x6.sum = sum_multi_value(x2, x6, multiple_linear_regression->n);
+    sigma_x->x2x6.sigma_of_sigma = sigma_x1x2(sigma_x->x2x6.sum, sigma_x->x2.sum, sigma_x->x6.sum, multiple_linear_regression->n);
+
+    sigma_x->x3x4.sum = sum_multi_value(x3, x4, multiple_linear_regression->n);
+    sigma_x->x3x4.sigma_of_sigma = sigma_x1x2(sigma_x->x3x4.sum, sigma_x->x3.sum, sigma_x->x4.sum, multiple_linear_regression->n);
+
+    sigma_x->x3x5.sum = sum_multi_value(x3, x5, multiple_linear_regression->n);
+    sigma_x->x3x5.sigma_of_sigma = sigma_x1x2(sigma_x->x3x5.sum, sigma_x->x3.sum, sigma_x->x5.sum, multiple_linear_regression->n);
+
+    sigma_x->x3x6.sum = sum_multi_value(x3, x6, multiple_linear_regression->n);
+    sigma_x->x3x6.sigma_of_sigma = sigma_x1x2(sigma_x->x3x6.sum, sigma_x->x3.sum, sigma_x->x6.sum, multiple_linear_regression->n);
+
+    sigma_x->x4x5.sum = sum_multi_value(x4, x5, multiple_linear_regression->n);
+    sigma_x->x4x5.sigma_of_sigma = sigma_x1x2(sigma_x->x4x5.sum, sigma_x->x4.sum, sigma_x->x5.sum, multiple_linear_regression->n);
+
+    sigma_x->x4x6.sum = sum_multi_value(x4, x6, multiple_linear_regression->n);
+    sigma_x->x4x6.sigma_of_sigma = sigma_x1x2(sigma_x->x4x6.sum, sigma_x->x4.sum, sigma_x->x6.sum, multiple_linear_regression->n);
+
+    sigma_x->x5x6.sum = sum_multi_value(x5, x6, multiple_linear_regression->n);
+    sigma_x->x5x6.sigma_of_sigma = sigma_x1x2(sigma_x->x5x6.sum, sigma_x->x5.sum, sigma_x->x6.sum, multiple_linear_regression->n);
 
     sigma_xy->x1y_sum = sum_multi_value(x1, y, multiple_linear_regression->n);
     sigma_xy->x1y_sigma_of_sigma = sigma_x1x2(sigma_xy->x1y_sum, sigma_x->x1.sum, sigma_y->y.sum, multiple_linear_regression->n);
@@ -73,9 +100,37 @@ int linear_regression(const double *x1, const double *x2, double *y, struct mult
     sigma_xy->x2y_sum = sum_multi_value(x2, y, multiple_linear_regression->n);
     sigma_xy->x2y_sigma_of_sigma = sigma_x1x2(sigma_xy->x2y_sum, sigma_x->x2.sum, sigma_y->y.sum, multiple_linear_regression->n);
 
-    multiple_linear_regression->result.b0 = b0(multiple_linear_regression);
-    multiple_linear_regression->result.b1 = b1(multiple_linear_regression);
-    multiple_linear_regression->result.b2 = b2(multiple_linear_regression);
+    sigma_xy->x3y_sum = sum_multi_value(x3, y, multiple_linear_regression->n);
+    sigma_xy->x3y_sigma_of_sigma = sigma_x1x2(sigma_xy->x3y_sum, sigma_x->x3.sum, sigma_y->y.sum, multiple_linear_regression->n);
+
+    sigma_xy->x4y_sum = sum_multi_value(x4, y, multiple_linear_regression->n);
+    sigma_xy->x4y_sigma_of_sigma = sigma_x1x2(sigma_xy->x4y_sum, sigma_x->x4.sum, sigma_y->y.sum, multiple_linear_regression->n);
+
+    sigma_xy->x5y_sum = sum_multi_value(x5, y, multiple_linear_regression->n);
+    sigma_xy->x5y_sigma_of_sigma = sigma_x1x2(sigma_xy->x5y_sum, sigma_x->x5.sum, sigma_y->y.sum, multiple_linear_regression->n);
+
+    sigma_xy->x6y_sum = sum_multi_value(x6, y, multiple_linear_regression->n);
+    sigma_xy->x6y_sigma_of_sigma = sigma_x1x2(sigma_xy->x6y_sum, sigma_x->x6.sum, sigma_y->y.sum, multiple_linear_regression->n);
+
+    double matrix[NOK][NOK+1] = {
+            {sigma_x->x1.sigma_of_sigma, sigma_x->x1x2.sigma_of_sigma, sigma_x->x1x3.sigma_of_sigma, sigma_x->x1x4.sigma_of_sigma, sigma_x->x1x5.sigma_of_sigma, sigma_x->x1x6.sigma_of_sigma, sigma_xy->x1y_sigma_of_sigma},
+            {sigma_x->x1x2.sigma_of_sigma, sigma_x->x2.sigma_of_sigma, sigma_x->x2x3.sigma_of_sigma, sigma_x->x2x4.sigma_of_sigma, sigma_x->x2x5.sigma_of_sigma, sigma_x->x2x6.sigma_of_sigma, sigma_xy->x2y_sigma_of_sigma},
+            {sigma_x->x1x3.sigma_of_sigma, sigma_x->x2x3.sigma_of_sigma, sigma_x->x3.sigma_of_sigma, sigma_x->x3x4.sigma_of_sigma, sigma_x->x3x5.sigma_of_sigma, sigma_x->x3x6.sigma_of_sigma, sigma_xy->x3y_sigma_of_sigma},
+            {sigma_x->x1x4.sigma_of_sigma, sigma_x->x2x4.sigma_of_sigma, sigma_x->x3x4.sigma_of_sigma, sigma_x->x4.sigma_of_sigma, sigma_x->x4x5.sigma_of_sigma, sigma_x->x4x6.sigma_of_sigma, sigma_xy->x4y_sigma_of_sigma},
+            {sigma_x->x1x5.sigma_of_sigma, sigma_x->x2x5.sigma_of_sigma, sigma_x->x3x5.sigma_of_sigma, sigma_x->x4x5.sigma_of_sigma, sigma_x->x5.sigma_of_sigma, sigma_x->x5x6.sigma_of_sigma, sigma_xy->x5y_sigma_of_sigma},
+            {sigma_x->x1x6.sigma_of_sigma, sigma_x->x2x6.sigma_of_sigma, sigma_x->x3x6.sigma_of_sigma, sigma_x->x4x6.sigma_of_sigma, sigma_x->x5x6.sigma_of_sigma, sigma_x->x6.sigma_of_sigma, sigma_xy->x6y_sigma_of_sigma}
+    };
+
+    double gaussian_elimination[NOK];
+    gaussianElimination(matrix, (double *) &gaussian_elimination);
+
+    multiple_linear_regression->result.b1 = gaussian_elimination[0];
+    multiple_linear_regression->result.b2 = gaussian_elimination[1];
+    multiple_linear_regression->result.b3 = gaussian_elimination[2];
+    multiple_linear_regression->result.b4 = gaussian_elimination[3];
+    multiple_linear_regression->result.b5 = gaussian_elimination[4];
+    multiple_linear_regression->result.b6 = gaussian_elimination[5];
+    multiple_linear_regression->result.b0 = sigma_y->y.average - (multiple_linear_regression->result.b1 * sigma_x->x1.average) - (multiple_linear_regression->result.b2 * sigma_x->x2.average) - (multiple_linear_regression->result.b3 * sigma_x->x3.average) - (multiple_linear_regression->result.b4 * sigma_x->x4.average) - (multiple_linear_regression->result.b5 * sigma_x->x5.average) - (multiple_linear_regression->result.b6 * sigma_x->x6.average);
 
     multiple_linear_regression->result.standard_error_estimate = standard_error_of_estimate(multiple_linear_regression);
     multiple_linear_regression->result.r_squared = r_squared(multiple_linear_regression);
@@ -109,4 +164,21 @@ double r_squared(struct multiple_linear_regression *multiple_linear_regression){
     numerator = (b1 * sigma_x1y) + (b2 * sigma_x2y);
 
     return numerator / denominator;
+}
+/*
+ * Sigma (y_actual - y_predicted)^2
+ */
+double mean_absolute_error(const double *y, const double *y_hat, int n){
+    double sum = 0;
+    int i;
+
+    for(i = 0; i < n; i++){
+        sum += fabs(y[i] - y_hat[i]);
+    }
+
+    return sum / n;
+}
+
+double predict(struct multiple_linear_regression *multiple_linear_regression, double x1, double x2, double x3, double x4, double x5, double x6){
+    return multiple_linear_regression->result.b0 + (multiple_linear_regression->result.b1 * x1) + (multiple_linear_regression->result.b2 * x2) + (multiple_linear_regression->result.b3 * x3) + (multiple_linear_regression->result.b4 * x4) + (multiple_linear_regression->result.b5 * x5) + (multiple_linear_regression->result.b6 * x6);
 }
--- regression.h
+++ regression.h
@@ -1,19 +1,44 @@
 //
 // Created by Naufal Reky Ardhana on 21/11/22.
 //
-
+#define NOK 6
 #ifndef REGRESI_LINEAR_BERGANDA_REGRESSION_H
 #define REGRESI_LINEAR_BERGANDA_REGRESSION_H
+
 struct sum{
     double sum;
     double square_sum;
     double sigma_of_sigma;
+    double average;
 };
 
 struct sigma_x {
     struct sum x1;
     struct sum x2;
+    struct sum x3;
+    struct sum x4;
+    struct sum x5;
+    struct sum x6;
+
     struct sum x1x2;
+    struct sum x1x3;
+    struct sum x1x4;
+    struct sum x1x5;
+    struct sum x1x6;
+
+    struct sum x2x3;
+    struct sum x2x4;
+    struct sum x2x5;
+    struct sum x2x6;
+
+    struct sum x3x4;
+    struct sum x3x5;
+    struct sum x3x6;
+
+    struct sum x4x5;
+    struct sum x4x6;
+
+    struct sum x5x6;
 };
 
 struct sigma_y {
@@ -25,14 +50,27 @@ struct sigma_xy {
     double x1y_sigma_of_sigma;
     double x2y_sum;
     double x2y_sigma_of_sigma;
+    double x3y_sum;
+    double x3y_sigma_of_sigma;
+    double x4y_sum;
+    double x4y_sigma_of_sigma;
+    double x5y_sum;
+    double x5y_sigma_of_sigma;
+    double x6y_sum;
+    double x6y_sigma_of_sigma;
 };
 
 struct result{
     double b0;
     double b1;
     double b2;
+    double b3;
+    double b4;
+    double b5;
+    double b6;
     double standard_error_estimate;
     double r_squared;
+    double mean_absolute_error;
 };
 
 struct multiple_linear_regression {
@@ -44,14 +82,14 @@ struct multiple_linear_regression {
 };
 #endif //REGRESI_LINEAR_BERGANDA_REGRESSION_H
 
-double b1(struct multiple_linear_regression *multiple_linear_regression);
-
-double b2(struct multiple_linear_regression *multiple_linear_regression);
-
-double b0(struct multiple_linear_regression *multiple_linear_regression);
-
-int linear_regression(const double *x1, const double *x2, double *y, struct multiple_linear_regression *multiple_linear_regression);
+int linear_regression(const double *x1, const double *x2, const double *x3,
+                      const double *x4, const double *x5, const double *x6,
+                      double *y, struct multiple_linear_regression *multiple_linear_regression);
 
 double standard_error_of_estimate(struct multiple_linear_regression *multiple_linear_regression);
 
 double r_squared(struct multiple_linear_regression *multiple_linear_regression);
+
+double mean_absolute_error(const double *y, const double *y_hat, int n);
+
+double predict(struct multiple_linear_regression *multiple_linear_regression, double x1, double x2, double x3, double x4, double x5, double x6);
