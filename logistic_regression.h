f356f88feee78f158eb04b9298ac5e753a82dc9d
reky <me@reky.dev>
2022-12-14 02:53:25 +0700

    Add independent variables (6 variables)

---
+++ logistic_regression.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+// Vestibulum commodo. Ut rhoncus gravida arcu.
+
+//
+// Created by Naufal Reky Ardhana on 23/11/22.
+//
+
+#ifndef REGRESI_LINEAR_BERGANDA_LOGISTIC_REGRESSION_H
+#define REGRESI_LINEAR_BERGANDA_LOGISTIC_REGRESSION_H
+
+#endif //REGRESI_LINEAR_BERGANDA_LOGISTIC_REGRESSION_H
